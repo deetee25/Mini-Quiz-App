244fa5a1b294d62ca25541e38aafa01fcd38ce51
David Thompson <deetee81@gmail.com>
2020-08-13 13:50:06 +0000

    taking objects, variables and arrays that will be used later

--- js/game.js
+++ js/game.js
@@ -1,3 +1,9 @@
 const ques = document.getElementById('ques');
 const options = Array.from(document.getElementsByClassName('option-text'));
-console.log(options);
+
+let currentques = {};
+let acceptingAnswers = false;
+let score = 0;
+let quesCounter = 0;
+let availableQuesions = []; 
+let quess = [];
